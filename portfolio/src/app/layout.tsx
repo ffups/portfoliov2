d01735eff71aebf880a7a88c4f7765996635d449
ffups <142579057+ffups@users.noreply.github.com>
2025-02-14 12:03:50 +0100

    refactor

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/components/NavBar'
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Joseph Silex",
@@ -25,13 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >        <header> <Navbar /></header>
-
+      <body>        
+        <header> <Navbar /></header>
         {children}
         <footer> Footer</footer>
-
       </body>
     </html>
   );
